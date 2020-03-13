1a606f87f87d324d414b9363d454245d9bdbf540
Justin Byun <klofseattle@hotmail.com>
2020-03-12 20:37:03 -0700

    Use react-hook-form for form validation and such

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,6 @@ import { Switch, Route, Redirect, Link } from 'react-router-dom';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 
--- src/store/GuestBook.ts
+++ src/store/GuestBook.ts
@@ -1,18 +1,45 @@
-// implement guestbookModel interface
-// import { action } from 'easy-peasy';
+import { action, thunk } from 'easy-peasy';
 import GuestBookModel from '../interfaces/GuestBookModel';
 
 const GuestBook: GuestBookModel = {
   entries: [
-    {
-      name: 'CJ',
-      content: 'hello World',
-      submitted: new Date(),
-    },
+    // {
+    //   id: Date.now(),
+    //   name: 'CJ',
+    //   content: 'hello World',
+    //   submitted: new Date(),
+    // },
   ],
-  // addEntry: action((state, payload) => {
-  //   state.entries.push(payload);
-  // }),
+  setEntries: action((state, entries) => {
+    state.entries = entries;
+  }),
+  addEntry: action((state, entry) => {
+    state.entries.unshift(entry);
+  }),
+  createEntry: thunk(async (state, entry) => {
+    // will be handled by back end
+    const response = await fetch('http://localhost:5000/entries', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(entry),
+    });
+    const result = await response.json();
+    // console.log(result);
+    state.addEntry(result);
+
+    // handled by front end
+    // entry.id = Date.now();
+    // entry.submitted = new Date();
+    // state.entries.unshift(entry);
+  }),
+  // get data from API using thunk (coming with easy-peasy) as it is async
+  getEntries: thunk(async state => {
+    const response = await fetch('http://localhost:5000/entries');
+    const entries = await response.json();
+    state.setEntries(entries);
+  }),
 };
 
 export default GuestBook;
